1668a9f1f250ae8aaab08211d8c82c1f1e02ebe7
shijia <1623299667@qq.com>
2020-01-02 10:44:13 +0800

    运行一个hello world

--- oauth2-demo/src/main/java/com/example/oauth2demo/Oauth2DemoApplication.java
+++ oauth2-demo/src/main/java/com/example/oauth2demo/Oauth2DemoApplication.java
@@ -2,12 +2,19 @@ package com.example.oauth2demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Oauth2DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Oauth2DemoApplication.class, args);
     }
 
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello";
+    }
 }
