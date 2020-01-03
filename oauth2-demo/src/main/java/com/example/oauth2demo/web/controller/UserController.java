7894884db59a31a781e4bdfea520609df19573a8
shijia <1623299667@qq.com>
2020-01-03 17:37:05 +0800

    重构代码

--- oauth2-demo/src/main/java/com/example/oauth2demo/web/controller/UserController.java
+++ oauth2-demo/src/main/java/com/example/oauth2demo/web/controller/UserController.java
@@ -6,18 +6,16 @@ import com.example.oauth2demo.dto.UserQueryCondition;
 import com.fasterxml.jackson.annotation.JsonView;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 @RestController
+@RequestMapping("/user")
 public class UserController {
-    @GetMapping("/user")
+    @GetMapping
     @JsonView(User.UserSimpleView.class)
     public List<User> query(UserQueryCondition userQueryCondition, Pageable pageable){
         log.info(JSON.toJSONString(userQueryCondition));
@@ -29,7 +27,7 @@ public class UserController {
         return users;
     }
 
-    @GetMapping("/user/{id:\\d+}")
+    @GetMapping("/{id:\\d+}")
     @JsonView(User.UserDetailView.class)
     public User getInfo(@PathVariable String id){
         User user = new User();
