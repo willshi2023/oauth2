9d9dd0c10b45b1ac053d550e384d24f7249e10e2
shijia <1623299667@qq.com>
2020-01-11 11:28:53 +0800

    使用多线程提高rest服务性能
    使用DeferredResult

--- oauth2-demo/src/main/java/com/example/oauth2demo/web/async/AsyncController.java
+++ oauth2-demo/src/main/java/com/example/oauth2demo/web/async/AsyncController.java
@@ -1,27 +1,39 @@
 package com.example.oauth2demo.web.async;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.RandomStringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
 
 import java.util.concurrent.Callable;
 
 @RestController
 @Slf4j
 public class AsyncController {
-    @GetMapping("/order")
-    public Callable<String> order() throws InterruptedException {
-        log.info("主线程开始");
-        Callable<String> result = new Callable<String>() {
+    @Autowired
+    private MockQueue mockQueue;
+    @Autowired
+    private DeferredResultHolder deferredResultHolder;
 
-            @Override
-            public String call() throws Exception {
-                log.info("副线程开始");
-                Thread.sleep(1000);
-                log.info("副线程返回");
-                return "success";
-            }
-        };
+    @GetMapping("/order")
+    public DeferredResult<String> order() {
+        log.info("主线程开始");
+        String orderNumber = RandomStringUtils.randomNumeric(8);
+        DeferredResult<String> result = new DeferredResult<>();
+        deferredResultHolder.getMap().put(orderNumber,result);
+        mockQueue.setPlaceOrder(orderNumber);
+//        Callable<String> result = new Callable<String>() {
+//
+//            @Override
+//            public String call() throws Exception {
+//                log.info("副线程开始");
+//                Thread.sleep(1000);
+//                log.info("副线程返回");
+//                return "success";
+//            }
+//        };
         log.info("主线程返回");
         return result;
     }
---
+++ oauth2-demo/src/main/java/com/example/oauth2demo/web/async/QueueListener.java
@@ -0,0 +1,38 @@
+package com.example.oauth2demo.web.async;
+
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class QueueListener implements ApplicationListener<ContextRefreshedEvent> {
+    @Autowired
+    private MockQueue mockQueue;
+    @Autowired
+    private DeferredResultHolder deferredResultHolder;
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent event) {
+        new Thread(()->{
+            while(true){
+                if(StringUtils.isNotBlank(mockQueue.getCompleteOrder())){
+                    String orderNumber = mockQueue.getCompleteOrder();
+                    log.info("返回订单处理的结果: {}",orderNumber);
+                    deferredResultHolder.getMap().get(orderNumber).setResult("place order success");
+                    mockQueue.setCompleteOrder(null);
+                }else {
+                    try {
+                        Thread.sleep(100);
+                    } catch (InterruptedException e) {
+                        log.error("error is: {}",e.getMessage());
+                    }
+                }
+            }
+        }).start();
+    }
+}
