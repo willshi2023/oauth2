b3909a17acc5c46eef37236e6377735cf13582d5
shijia <1623299667@qq.com>
2020-01-12 15:20:16 +0800

    自定义用户认证
    处理加密解密

--- oauth2-browser/src/main/java/com/example/oauth2browser/MyUserDetailService.java
+++ oauth2-browser/src/main/java/com/example/oauth2browser/MyUserDetailService.java
@@ -1,21 +1,28 @@
 package com.example.oauth2browser;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class MyUserDetailService implements UserDetailsService {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         log.info("登录用户名: {}",username);
 //        根据用户查找用户信息
-        return new User(username,"123456", AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
+        String password = passwordEncoder.encode("123456");
+        log.info("从数据库读取的密码是: {}",password);
+        return new User(username, password, AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
     }
 }
