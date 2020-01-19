3a20a4d912a596e11d1be0940c570ffe0eb381c6
shijia <1623299667@qq.com>
2020-01-19 16:10:31 +0800

    自定义用户认证逻辑
    处理用户校验逻辑

--- oauth2-browser/src/main/java/com/example/oauth2browser/MyUserDetailService.java
+++ oauth2-browser/src/main/java/com/example/oauth2browser/MyUserDetailService.java
@@ -30,6 +30,19 @@ public class MyUserDetailService implements UserDetailsService {
 //        根据用户查找用户信息
         String password = passwordEncoder.encode("123456");
         log.info("从数据库读取的密码是: {}",password);
+        /*
+        用户校验逻辑是通过userdetails 的一些方法进行设置值然后被框架本身校验，分别如下：
+            boolean isAccountNonExpired();  账户未过期
+            boolean isAccountNonLocked();   账户未被锁定
+            boolean isCredentialsNonExpired();  密码未过期
+            boolean isEnabled();    账户可用
+            任何一项设设置成false都将不通过认证。返回的对象可以是springsecurity提供的user对象，也可以是任意的自定义的实现了UserDetails的对象
+            user对象还有一个构造方法，可以直接用来设置用户逻辑校验
+            public User(String username, String password, boolean enabled,
+			boolean accountNonExpired, boolean credentialsNonExpired,
+			boolean accountNonLocked, Collection<? extends GrantedAuthority> authorities)
+			对于不通过的地方填false就行了
+         */
         return new User(username, password, AuthorityUtils.commaSeparatedStringToAuthorityList("admin"));
     }
 }
